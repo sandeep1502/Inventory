f3549d568809c15226df04dbfeb17d9e6c511e42
pennantstaff <pennantstaff@PENNANT-VSP85>
2019-11-05 16:56:51 +0530

    register

---
+++ inven/src/main/java/com/inven/inven/registerBean.java
@@ -0,0 +1,44 @@
+package com.inven.inven;
+
+public class registerBean {
+	
+	private String username;
+	private String type;
+	private String email;
+	private String password;
+	private String cPassword;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getcPassword() {
+		return cPassword;
+	}
+	public void setcPassword(String cPassword) {
+		this.cPassword = cPassword;
+	}
+	
+	
+	
+
+}
